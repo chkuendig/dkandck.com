e76c065a851aeadf8b883112279820161b9e3084
Christian Kündig <christian@kuendig.info>
2024-05-27 22:04:20 +0200

    Add WebWorldWind as submodule

--- update-wwd.sh
+++ update-wwd.sh
@@ -1,2 +1,4 @@
-cp ../WebWorldWind/build/dist/worldwind.min.js  js/
-cp ../WebWorldWind/build/dist/worldwind.js  js/
+cd ./WebWorldWind/ && npm install && npm run build
+cd ..
+cp ./WebWorldWind/build/dist/worldwind.min.js  js/
+cp ./WebWorldWind/build/dist/worldwind.js  js/
